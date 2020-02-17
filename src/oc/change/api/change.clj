46b610515455a2d90834bd61090ec9a5be80e41f
bago2k4 <iacopo@rc0.eu>
2020-02-17 16:45:01 +0100

    Add delete method to post-read resource to delete a read record.

--- src/oc/change/api/change.clj
+++ src/oc/change/api/change.clj
@@ -2,12 +2,23 @@
   "Liberator API for change data."
   (:require [if-let.core :refer (if-let*)]
             [liberator.core :refer (defresource by-method)]
-            [compojure.core :as compojure :refer (GET OPTIONS)]
+            [compojure.core :as compojure :refer (DELETE GET OPTIONS)]
             [cheshire.core :as json]
             [oc.lib.api.common :as api-common]
             [oc.change.resources.seen :as seen]
             [oc.change.resources.read :as read]
             [oc.change.config :as config]))
+
+(defn- notify-watchers-of-unread!
+  "Send a message to all the watcher of this post's container so they reload the read data."
+  [container-id post-id]
+  (when container-id
+    (let [read-data (read/retrieve-by-item post-id)
+          status {:item-id post-id :reads read-data}]
+      (>!! watcher/watcher-chan {:send true
+                                 :watch-id container-id
+                                 :event :item/status
+                                 :payload status}))))
 
 ;; Representations
 (defn- render-post-change [post]
@@ -18,19 +29,21 @@
 (defresource post-read [post-uuid]
   (api-common/open-company-authenticated-resource config/passphrase) ; verify validity and presence of required JWToken
 
-  :allowed-methods [:options :get]
+  :allowed-methods [:options :get :delete]
 
   ;; Authorization
   :allowed? (by-method {
     :options true
     :get true
+    :delete true
   })
 
-  :available-media-types (by-method {:get nil})
+  :available-media-types (by-method {:delete ["text/plain"] :get nil})
 
   :known-content-type? (by-method {
     :options true
     :get true
+    :delete true
   })
 
   ;; Existentialism
@@ -41,6 +54,17 @@
                                                :read read-data}}
                         false))
 
+  ;; Delete
+  :delete! (fn [ctx] (let [user (:user ctx)
+                           user-id (:user-id user)
+                           delete-item (read/delete! post-uuid user-id)
+                           container-id (-> ctx :request :body slurp)]
+                       (if (nil? delete-item)
+                         (do
+                           (notify-watchers-of-unread! container-id post-uuid)
+                           {:deleted-items :ok})
+                         {:deleted-items false})))
+
   ;; Responses
   :handle-ok (fn [ctx] (render-post-change (:existing-post ctx))))
 
@@ -50,4 +74,6 @@
     (OPTIONS "/change/read/post/:post-uuid" [post-uuid] (post-read post-uuid))
     (OPTIONS "/change/read/post/:post-uuid/" [post-uuid] (post-read post-uuid))
     (GET "/change/read/post/:post-uuid" [post-uuid] (post-read post-uuid))
-    (GET "/change/read/post/:post-uuid/" [post-uuid] (post-read post-uuid))))
+    (GET "/change/read/post/:post-uuid/" [post-uuid] (post-read post-uuid))
+    (DELETE "/change/read/post/:post-uuid" [post-uuid] (post-read post-uuid))
+    (DELETE "/change/read/post/:post-uuid/" [post-uuid] (post-read post-uuid))))
