fb68c21e4c6f268cbf332bb2ae0d9a5aa6b24925
bago2k4 <iacopo@rc0.eu>
2021-02-12 20:43:18 +0100

    Fix oc-lib deps exclusions.

--- project.clj
+++ project.clj
@@ -46,7 +46,7 @@
     ;; ***************** (JWT schema changes, more info here: *****************
     ;; ******* https://github.com/open-company/open-company-lib/pull/82) ******
     ;; ************************************************************************
-    [open-company/lib "0.17.31-alpha6" :exclusions [ring/ring-core com.amazonaws/aws-java-sdk-dynamodb commons-codec joda-time com.taoensso/encore]]
+    [open-company/lib "0.17.31-alpha6" :exclusions [ring/ring-core commons-codec org.clojure/tools.reader]]
     ;; ************************************************************************
     ;; In addition to common functions, brings in the following common dependencies used by this project:
     ;; httpkit - Web server http://http-kit.org/
